2cdd7b4e7cdcab78bf65faa1de7883c766604989
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-06-22 09:22:16 +0300

    Update source.php

--- source.php
+++ source.php
@@ -7,6 +7,10 @@ $update=json_decode($data,true);
 $message=$update["message"];
 $text=$message["text"];
 $cid=$update["message"]["from"]["id"];
+$from = $message["from"];
+$username = $from["username"];
+$name= $from["name"];
+$cogname = $from["cogname"];
 
 
 function apiRequest($metodo)
@@ -21,5 +25,13 @@ function send($id,$text)
 	}
 	return apiRequest("sendMessage?text=$text&parse_mode=HTML&chat_id=$id");
 }
+// function keyboard($tasti,$text,$cd){
+// $tasti2=$tasti;
 
+// $tasti3=json_encode($tasti2);
+// 	if(strpos($text,"\n")){
+// 		$text= urlencode($text);
+// 	}
+// apiRequest("sendMessage?text=$text&parse_mode=Markdown&chat_id=$cd&reply_markup=$tasti3");
+// }
 ?>
