c39017aba98afca053c787ceff034938446ac65f
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-06-22 10:23:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 	if($text =="/start"){
 		// send($cid,"welcome @$username $name $cogname  to fila's bot .....");
 	   //@$username $name $cogname
-		$keyboard ={
+		$keyboard =[
 			["filla1","fillla2"],
 			["fillaa3","fillla4"],
-		};
+		];
 		$key=array("resize_keyboard"=>true,
 					"keyboard"=>$keyboard,);
 		keyboard($key,"fila aaaa aaaa",$cid);
