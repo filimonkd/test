98ed13720f2a70eb720fb2385384e6f01b728095
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-06-22 09:28:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	define('token','619433200:AAF0Oblwc2gsOgWHZk1NCo01MlRRj7AcgGQ');
 	include 'source.php';
 	if($text =="/start"){
-		send($cid,"welcome @$username @$name @$cogname  to fila's bot .....");
+		send($cid,"welcome @$username $nome @$cognome  to fila's bot .....");
 		//@$username $name $cogname
 	}
 ?>
