a70b0bbb83157171ec138272629fca8c7e5d0905
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-06-25 09:53:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,7 @@
 	define('token','602850117:AAFbCgH0FxBbz3gdSVifBiwzUdbIOWCZLDw');
 	include 'source.php';
 	if($text =="/start"){
-		 //send($cid,"hey @$username $name $cogname,wellcome  to fila's bot .....");
-	   //@$username $name $cogname
-		$keyboard =]
-			["filla1","fillla2"],
-			["fillaa3","fillla4"],
-		];
-		$key=array("resize_keyboard"=>true,
-					"keyboard"=>$keyboard,);
-		keyboard($key,"fila aaaa aaaa",$cid);
-		
+		send($cid,"fila gr8 gr8.....");
 	}
-
 ?>
+
