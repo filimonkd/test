e432a5d1843891f3f4cda03603bc5352b8995930
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-06-25 10:31:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,21 @@
 	define('token','602850117:AAFbCgH0FxBbz3gdSVifBiwzUdbIOWCZLDw');
 	include 'source.php';
 	if($text =="/start"){
-		 send($cid,"hey @$username $name $cogname,wellcome  to fila's bot .....");
+		// send($cid,"hey @$username $name $cogname,wellcome  to fila's bot .....");
 	   //@$username $name $cogname
-		
-		
-	}
-	if($text=="test"){
 		$keyboard =[]
-			["filla1","fillla2"],
+			["Inline","fillla2"],
 			["fillaa3","fillla4"],
 			["fillaa3"],
 		];
 		$key=array("resize_keyboard"=>true,
 					"keyboard"=>$keyboard,);
 		keyboard($key,"fila aaaa aaaa",$cid);
+		
+	}
+	if ($text=="Inline") {
+		$but=array(array(array("text"=>"btn1","url"="www.google.com"),),);
+		$inlinekeyboard($but,$cid,"click for google.com");
 	}
 
 ?>
