379a52880539aa9ceb01a2bf9d9d9fd00a6e38f9
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-06-25 10:31:54 +0300

    Update source.php

--- source.php
+++ source.php
@@ -32,6 +32,17 @@ $tasti3=json_encode($tasti2);
 	if(strpos($text,"\n")){
 		$text= urlencode($text);
 	}
-	return apiRequest("sendMessage?text=$text&parse_mode=Markdown&chat_id=$cd&reply_markup=$tasti3");
+apiRequest("sendMessage?text=$text&parse_mode=Markdown&chat_id=$cd&reply_markup=$tasti3");
+}
+function inlinekeyboard($menud,$chat,$text){
+	$menu=$menud;
+	if(strpos($text,"\n")){
+		$text= urlencode($text);
+	}
+	$d2=array(
+		"inline_keyboard" => $menu,);
+	$d2=json_encode($d2);
+	return apiRequest("sendMessage?chat_id=$chat&parse_mode=Markdown&text=$text&reply_markup=$d2");
+
 }
 ?>
