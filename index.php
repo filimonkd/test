9bb450c949eb9dcf0d453613a82d35b1e7f5a76c
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-06-25 09:35:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
 	define('token','619433200:AAF0Oblwc2gsOgWHZk1NCo01MlRRj7AcgGQ');
 	include 'source.php';
 	if($text =="/start"){
-		 send($cid,"hey @$username $name $cogname,wellcome  to fila's bot .....");
+		 //send($cid,"hey @$username $name $cogname,wellcome  to fila's bot .....");
 	   //@$username $name $cogname
-		
-	}
-	if ($text=="test") {
 		$keyboard =]
 			["filla1","fillla2"],
 			["fillaa3","fillla4"],
@@ -15,5 +12,7 @@
 		$key=array("resize_keyboard"=>true,
 					"keyboard"=>$keyboard,);
 		keyboard($key,"fila aaaa aaaa",$cid);
+		
 	}
+
 ?>
