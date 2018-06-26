7b09063d4aa5c520b58887b36dac38cd9b1e63b2
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-06-26 10:36:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 	define('token','575325886:AAHfc2X09sE7Av05BocjUCehRUpW8Cks9Lc');
 	include 'source.php';
 	if($text =="/start"){
-		send($cid,"fila gr8 gr8.....");
+		$keyboard =[
+			["Inline","Inline2"],
+			["Inline3","Inline4"],
+		];
+		$key=array("resize_keyboard"=>true,
+					"keyboard"=>$keyboard,);
+		keyboard($key,"fila aaaa aaaa",$cid);
 	}
 ?>
