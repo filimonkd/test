232c484204d74c1321aaa670207c1438b917f4a9
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-06-26 10:02:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,39 +2,39 @@
 	define('token','602850117:AAFbCgH0FxBbz3gdSVifBiwzUdbIOWCZLDw');
 	include 'source.php';
 	if($text =="/start"){
-		// send($cid,"hey @$username $name $cogname,wellcome  to fila's bot .....");
+	send($cid,"hey @$username $name $cogname,wellcome  to fila's bot .....");
 	   //@$username $name $cogname
-		$keyboard =[
-			["Inline","Inline2"],
-			["Inline3","Inline4"],
-			["fillaa3"],
-		];
-		$key=array("resize_keyboard"=>true,
-					"keyboard"=>$keyboard,);
-		keyboard($key,"fila aaaa aaaa",$cid);
+// 		$keyboard =[
+// 			["Inline","Inline2"],
+// 			["Inline3","Inline4"],
+// 			["fillaa3"],
+// 		];
+// 		$key=array("resize_keyboard"=>true,
+// 					"keyboard"=>$keyboard,);
+// 		keyboard($key,"fila aaaa aaaa",$cid);
 		
 	}
-	if ($text=="Inline") {
-		$but=array(array(array("text"=>"btn1","url"="www.google.com"),),);
-		$inlinekeyboard($but,$cid,"click for google.com");
-	}
-	if ($text=="Inline2") {
-		$but=array(array(array("text"=>"btn1","url"="www.google.com"),array("text"=>"btn1","url"="www.google.com"),),);
-		$inlinekeyboard($but,$cid,"click for google.com");
-	}
-	if ($text=="Inline3") {
-		$but[]=array(array("text"=>"btn1","url"="www.google.com"),);
-		$but[]=array(array("text"=>"btn2","url"="www.google.com"),);
-		$inlinekeyboard($but,$cid,"click for google.com");
-	}
-	if ($text=="Inline4") {
-		$but=array(array(array("text"=>"btn1","callback_data"="ccccc"),),);
-		$inlinekeyboard($but,$cid,"click for google.com");
-	}
-	if (callback($update)) {
-		if ($cbdata=="ccccc") {
-			send($cbid,"test sss");
-		}
-	}
+// 	if ($text=="Inline") {
+// 		$but=array(array(array("text"=>"btn1","url"="www.google.com"),),);
+// 		$inlinekeyboard($but,$cid,"click for google.com");
+// 	}
+// 	if ($text=="Inline2") {
+// 		$but=array(array(array("text"=>"btn1","url"="www.google.com"),array("text"=>"btn1","url"="www.google.com"),),);
+// 		$inlinekeyboard($but,$cid,"click for google.com");
+// 	}
+// 	if ($text=="Inline3") {
+// 		$but[]=array(array("text"=>"btn1","url"="www.google.com"),);
+// 		$but[]=array(array("text"=>"btn2","url"="www.google.com"),);
+// 		$inlinekeyboard($but,$cid,"click for google.com");
+// 	}
+// 	if ($text=="Inline4") {
+// 		$but=array(array(array("text"=>"btn1","callback_data"="ccccc"),),);
+// 		$inlinekeyboard($but,$cid,"click for google.com");
+// 	}
+// 	if (callback($update)) {
+// 		if ($cbdata=="ccccc") {
+// 			send($cbid,"test sss");
+// 		}
+// 	}
 
 ?>
