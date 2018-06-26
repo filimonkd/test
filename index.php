d88a7a2e07928ee83e8c5e0df7760897f262be5b
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-06-26 10:21:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 	define('token','575325886:AAHfc2X09sE7Av05BocjUCehRUpW8Cks9Lc');
 	include 'source.php';
 	if($text =="/start"){
+		send($cid,"hey @$username $name $cogname,wellcome  to fila's bot .....");
+	   
+		
+	}
+	if($text =="test"){
 		// send($cid,"hey @$username $name $cogname,wellcome  to fila's bot .....");
 	   //@$username $name $cogname
 		$keyboard =[
