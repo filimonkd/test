0a36821a4cb57a2494a91691e9852697b1bc0c8b
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-07-05 14:19:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,16 @@
 		
 	}
 	if ($text=="Inline") {
-		$but=array(array(array("text"=>"btn1","url"=>"www.google.com"),),);
-		inlinekeyboard($but,$cid,"click for google.com");
+// 		$but=array(array(array("text"=>"btn1","url"=>"www.google.com"),),);
+// 		inlinekeyboard($but,$cid,"click for google.com");
+		$keyboard =[
+			["Inline555","Inline2"],
+			["Inline3","Inline4"],
+			["fillaa3"],
+		];
+		$key=array("resize_keyboard"=>true,
+					"keyboard"=>$keyboard,);
+		keyboard($key,"fila aaaa aaaa",$cid);
 	}
 	if ($text=="Inline2") {
 		$but=array(array(array("text"=>"btn1","url"=>"www.google.com"),array("text"=>"btn1","url"=>"www.google.com"),),);
