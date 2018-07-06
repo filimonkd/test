aa1427b10528c05052d7323346777ef1c44d1bee
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-07-06 11:35:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
-
-// Script to test if the CURL extension is installed on this server
-
-// Define function to test
-function _is_curl_installed() {
-    if  (in_array  ('curl', get_loaded_extensions())) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-// Ouput text to user based on test
-if (_is_curl_installed()) {
-  echo "cURL is <span style=\"color:blue\">installed</span> on this server";
-} else {
-  echo "cURL is NOT <span style=\"color:red\">installed</span> on this server";
-}
+$INSTANCE_ID = 'YOUR_INSTANCE_ID_HERE';  // TODO: Replace it with your gateway instance ID here
+$CLIENT_ID = "YOUR_CLIENT_ID_HERE";  // TODO: Replace it with your Premium client ID here
+$CLIENT_SECRET = "YOUR_CLIENT_SECRET_HERE";   // TODO: Replace it with your Premium client secret here
+$postData = array(
+  'number' => '12025550108',  // Specify the recipient's number (NOT the gateway number) here.
+  'message' => 'Have a nice day! Loving you:)'  // FIXME
+);
+$headers = array(
+  'Content-Type: application/json',
+  'X-WM-CLIENT-ID: '.$CLIENT_ID,
+  'X-WM-CLIENT-SECRET: '.$CLIENT_SECRET
+);
+$url = 'http://api.whatsmate.net/v1/telegram/single/message/' . $INSTANCE_ID;
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+$response = curl_exec($ch);
+echo "Response: ".$response;
+curl_close($ch);
 ?>
