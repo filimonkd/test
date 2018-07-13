e0e383d17265f5668db7f6b510f161cdd56e559e
filimonkd <40231410+filimonkd@users.noreply.github.com>
2018-07-13 10:30:26 +0300

    Update source.php

--- source.php
+++ source.php
@@ -42,11 +42,11 @@ function send_photo1($id)
 }
 function send_document($id)
 {
-	return apiRequest("senddocument?chat_id=$id&document=BQADBAADcgMAAlO2QFJF8dzXHscgogI");
+	return apiRequest("senddocument?chat_id=$id&document=BQADBAADcAMAAlO2QFJ3kzNG26rvawI");
 }
 function send_document1($id)
 {
-	return apiRequest("senddocument?chat_id=$id&document=BQADBAADcAMAAlO2QFJ3kzNG26rvawI");
+	return apiRequest("senddocument?chat_id=$id&document=BQADBAADcgMAAlO2QFJF8dzXHscgogI");
 }
 function send_video($id)
 {
